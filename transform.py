4375b6e1911b8fbd353a0a1ce93ffac02c11be00
himanshusingh1412 <himanshusingh1412>
2024-01-24 19:39:51 +0530

    test

--- transform.py
+++ transform.py
@@ -17,10 +17,9 @@ def filter_students_by_department(df, department):
     return filtered_df
 
 def transform(student_df):
-    df=student_df
-    student_df = add_gpa_column(df)
-    student_df = categorize_students_by_age(df)
-    filtered_students = filter_students_by_department(df, 'Computer Science')
+    student_df = add_gpa_column(student_df)
+    student_df = categorize_students_by_age(student_df)
+    filtered_students = filter_students_by_department(student_df, 'Computer Science')
 
     print("Original DataFrame:")
     print(student_df)
